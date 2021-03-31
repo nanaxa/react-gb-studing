41b189c332866bf4e202aa2fe1ca19390b9b70c6
nanaxa <pavpildan@gmail.com>
2021-03-31 23:57:48 +0700

    Counter state is declared

--- src/components/timer.js
+++ src/components/timer.js
@@ -1,9 +1,17 @@
 import React, {Component} from "react";
 
 export class Counter extends Component {
+        constructor(props) {
+            super(props);
+            this.state = {
+                count: 5
+            }
+
+        }
     //we do when component mount
     componentDidMount() {
         console.log("Я компонент и я загрузился");
+        this.setState({count:52});
     }
     //this is bad practice to use componentWillMount
    /* componentWillMount() {
@@ -17,7 +25,14 @@ export class Counter extends Component {
     componentWillUnmount() {
          console.log("Я компонент и я буду отключен");
     }
+    handleCount() {
+            console.log("There is a tap");
+        }
     render() {
-        return <span>It's counter</span>;
+        return <>
+                <span>{this.state.count}</span>
+                <button type="submit" onClick={this.handleCount}>tap</button>
+                </>
     }
 }
+
