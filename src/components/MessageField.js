cf299aaaa719ae7f368a8cf10f10e7ca875843b1
nanaxa <pavpildan@gmail.com>
2021-04-13 16:28:07 +0700

    add auto answer after 1 seconds

--- src/components/MessageField.js
+++ src/components/MessageField.js
@@ -4,10 +4,18 @@ import {Message} from "./message";
 
 export class MessageField extends Component {
         state = {
-            messages: ["Hi", "What's up?"],
+            messages: ["Hi!", "What's up?"],
         }
 
-        handleClick = () => {
+        componentDidUpdate() {
+            if (this.state.messages.length % 2 === 1) {
+                setTimeout(() => {
+                    this.setState({messages: [...this.state.messages, "Do not Flood! I'm a Robot!"]})
+                }, 1000)
+            }
+        }
+
+    handleClick = () => {
             this.setState({messages: [ ...this.state.messages, "Cool!" ]})
         }
 
